2aaf48e07432a0c127c4199bd1f1cfe65aa00591
matt smith <matt@matts-MacBook-Pro-2.local>
2019-02-25 10:23:57 +0000

    added 404 not found handler

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student/{id}", name="student_show")
+     */
+    public function show($id)
+    {
+        $studentRepository = new StudentRepository();
+        $student = $studentRepository->find($id);
+
+        $template = 'student/show.html.twig';
+        $args = [
+            'student' => $student
+        ];
+
+        // if NULL from find()
+        if (!$student) {
+            $template = 'error/404.html.twig';
+        }
+
+        return $this->render($template, $args);
+    }
+
+    /**
+     * @Route("/student", name="student_list")
+     */
+    public function listAction()
+    {
+        $studentRepository = new StudentRepository();
+        $students = $studentRepository->findAll();
+
+        $template = 'student/list.html.twig';
+        $args = [
+            'students' => $students
+        ];
+        return $this->render($template, $args);
+    }
+}
